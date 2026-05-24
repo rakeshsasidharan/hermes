30596e5cbcfa09aac5583b24a28f721156589db7
rakeshpillai <rakesh.sasidharan@gmail.com>
2026-05-24 13:25:38 -0700

    [fix]:replace sonnar api with shadcn wrapper for uniform theming

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import { ThemeProvider } from '@/components/theme-provider';
-import { Toaster } from 'sonner';
+import { Toaster } from '@/components/ui/sonner';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,7 +24,7 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
-          <Toaster richColors closeButton />
+          <Toaster closeButton />
         </ThemeProvider>
       </body>
     </html>
